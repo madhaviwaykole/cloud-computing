31ea5ebea0daff81b2ea2aa49e0f4054c3d535e8
madhaviwaykole <42990392+madhaviwaykole@users.noreply.github.com>
2019-08-13 15:38:35 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
+# Copyright (c), bhupendra - cdac, 2019, August,6,2019
 from flask import Flask
 application = Flask(__name__)
+
 @application.route('/')
 def hello_world():
-  return "hello world in python, run as a POD in openShift \r\n",200,{'Content-Type':'text/plain'}
-if __name__=='__main__':
-  application.run(debug=True)
+    return "Hello World in Python, Run as a POD in OpenShift\r\n", 200, { 'Content-Type': 'text/plain' }
+
+if __name__ == '__main__':
+    application.run(debug = True)
